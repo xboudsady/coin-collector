b9f8738ad1025d9cda0c09374b765a76905e78bd
Xayaseth Boudsady <boudsadyx@gmail.com>
2017-12-30 14:29:21 -0800

    Added poison, and star spritesheet'

--- js/game.js
+++ js/game.js
@@ -15,13 +15,34 @@ var winningScore = 100;
 // add collectable items to the game
 function addItems() {
     items = game.add.physicsGroup();
-    createItem(375, 300, 'coin');
+    createItem(375, 400, 'coin');
+    createItem(575, 500, 'coin');
+    createItem(225, 500, 'coin');
+    createItem(100, 250, 'coin');
+    createItem(575, 150, 'coin');
+    createItem(525, 300, 'coin');
+    createItem(650, 250, 'coin');
+    createItem(225, 200, 'coin');
+    createItem(375, 100, 'coin');
+    createItem(370, 500, 'poison');
+    createItem(100, 375, 'poison');
+    createItem(125, 50, 'star'); 
 }
 
 // add platforms to the game
 function addPlatforms() {
     platforms = game.add.physicsGroup();
     platforms.create(450, 550, 'platform');
+    platforms.create(100, 550, 'platform');
+    platforms.create(300, 450, 'platform');
+    platforms.create(250, 150, 'platform');
+    platforms.create(50, 300, 'platform');
+    platforms.create(150, 250, 'platform');
+    platforms.create(650, 300, 'platform');
+    platforms.create(550, 200, 'platform2');
+    platforms.create(300, 450, 'platform2');
+    platforms.create(400, 350, 'platform2');
+    platforms.create(100, 100, 'platform2');
     platforms.setAll('body.immovable', true);
 }
 
@@ -65,11 +86,14 @@ window.onload = function () {
 
         //Load images
         game.load.image('platform', 'asset/platform_1.png');
+        game.load.image('platform2', 'asset/platform_2.png');
 
         //Load spritesheets
         game.load.spritesheet('player', 'asset/chalkers.png', 48, 62);
         game.load.spritesheet('coin', 'asset/coin.png', 36, 44);
         game.load.spritesheet('badge', 'asset/badge.png', 42, 54);
+        game.load.spritesheet('poison', 'asset/poison.png', 32, 32);
+        game.load.spritesheet('star', 'asset/star.png', 32, 32);
     }
 
     // initial game set up
